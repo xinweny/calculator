e18d136b395738b83595e5757f02d53ae8d885e9
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-30 12:35:59 -0400

    Add operate function

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(operator, a, b) {
+  switch(operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
