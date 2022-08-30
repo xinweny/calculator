899b9a8f8a8caa544c83affdda2f90c44eb31a4e
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-08-30 12:17:08 -0400

    Add basic operation functions

---
+++ main.js
@@ -0,0 +1,17 @@
+// Functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
