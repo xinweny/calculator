ef6876478a4d09e6f45ff048c51082aa6c1e6aaf
Xin Wen Yap <yapxinwen1608@gmail.com>
2022-09-01 01:02:34 -0400

    Set decimal places limit to 5

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
     decimalButton: document.querySelector('.decimal-button'),
     deleteButton: document.querySelector('.delete-button'),
     keyStrokes: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/', '=', 'C', 'c', 'Backspace', 'Enter'],
-    decPlaces: 7
+    decPlaces: 5
   }
 
   const state = {
